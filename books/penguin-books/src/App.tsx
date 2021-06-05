c388189dbd472092ff19986bd9141287ae87bbb9
Anirudh Janga <j.anirudh@icloud.com>
2021-06-05 13:29:14 -0500

    adding basic content

---
+++ books/penguin-books/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">Penguin Book House</header>
+            <BrowserRouter></BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
